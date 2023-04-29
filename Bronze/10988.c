4090e45025d682310d88a822f9f9bd813641053f
KinsHishI <jeongmin9011@knu.ac.kr>
2023-04-29 22:39:34 +0900

    commited

---
+++ Bronze/10988.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEGNTH 100
+
+int main(void) {
+	char str[MAX_LEGNTH];
+	int result = 1;
+
+	scanf("%s", str);
+
+	for (int i = 0; i < strlen(str) / 2; i++) {
+		if (str[i] != str[strlen(str) - 1 - i])
+			result = 0;
+	}
+
+	if (result)
+		printf("1");
+	else
+		printf("0");
+
+	return 0;
+}
