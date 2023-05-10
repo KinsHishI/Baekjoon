4352cfee83046d076f8591457f271e987fff8793
KinsHishI <jeongmin9011@knu.ac.kr>
2023-05-10 20:48:25 +0900

    commited

---
+++ Bronze/11005.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void convert(int N, int B) {
+	if (N != 0) 	{
+		convert(N / B, B);
+		if (N % B > 9 && B > 10)
+			printf("%c", 'A' + N % B - 10);
+		else
+			printf("%d", N % B);
+	}
+}
+
+int main(void) {
+	int N, B;
+
+	scanf("%d %d", &N, &B);
+
+	convert(N, B);
+
+	return 0;
+}
